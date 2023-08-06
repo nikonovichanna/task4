e2564c224bafc2ff29dfb55e606107821df7ff72
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-07 01:07:27 +0300

    add task4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int num = new Random().Next(1,20);
+Console.WriteLine(num);
+int start = 1;
+while (start <= num) 
+{
+      if (start % 2 == 0)
+      {
+        Console.WriteLine(start);
+      }
+start++;      
+}
+   
